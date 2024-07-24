71adc606f4cdcf9a9763df7b0e620e4f1e65edcc
Malak Abdalla <malakoabdalla@gmail.com>
2024-07-24 14:13:24 -0500

    computed order for n=8,10,12

--- lib/utils.py
+++ lib/utils.py
@@ -1,4 +1,4 @@
-from sage.combinat.permutation import Permutation
+from sage.combinat.permutation import Permutation #, SymmetricGroup
 
 
 def all_transpositions(n):
@@ -21,7 +21,7 @@ def all_transpositions_of_elements(elements):
 
     return perms
 
-
+##this is an example of a for loop that goes through a list of numbers 
 def all_pairs(list_1, list_2):
     pairs = []
     for i in range(len(list_1)):
@@ -66,10 +66,22 @@ def compute_valid_perms(elements):
 
     return valid_perms
 
+# pair is a list of tuples
+# want to convert it to a Permutation type from the sage SymmetricGroup library
+# e.g. pair = [[(1,2),(3,4)], [(1,3), (2,4)]]
+# return (Permutation('(12)(34)'), Permutation('(14)(23)))
+def convert_pair(n, pair):
+    Sn = SymmetricGroup(n)
+    top = Sn(pair[0])
+    bottom = Sn(pair[1])
+    return (top,bottom)
+
+
+
 # we dont want there to be the same pairings ie cannot have T and B (14) and (14), dont wanna include 
 #goal is to output a list of pairs of permutations 
 #two permutations create a pair and check if they share any permutations, if they do share discard, dont.. keep
-def compute_valid_pairs(valid_perms):
+def compute_valid_pairs(n, valid_perms):
     valid_pairings= []
     for i,v in enumerate(valid_perms):
         for j,w in enumerate(valid_perms):
@@ -84,7 +96,8 @@ def compute_valid_pairs(valid_perms):
                 continue 
             else : 
                 # print(f"they're not equal, appending: {first_pair}")
-                valid_pairings.append(first_pair)
+                pair = convert_pair(n, first_pair)
+                valid_pairings.append(pair)
 
     return valid_pairings
 
@@ -99,3 +112,33 @@ def check_if_perms_share_transposition(perm1, perm2):
     
     return False 
         
+
+# This function takes in two inputs:
+# - n which is the number of dots (permutation group)
+# - bottom which is the bottom permutation 
+# It returns a permutation which is the shift of bottom
+
+def shift_bottom(n, bottom):
+    Sn = SymmetricGroup(n)
+    s=''
+    for i in range(1,n+1):    
+        if i==1:
+            s=s+'(1,'
+        elif i<n : 
+            s=s+ f'{n-i+2},'
+        if i==n:
+            s=s+ '2)'
+      
+    si= ''
+    for i in range(1,n+1):
+        if i==1:
+            si= si+'(1,'
+        elif i<n:
+            si= si+ f'{i},'
+        if i==n:
+            si= si+ f'{n})'
+   
+    s = Sn(s)
+    si = Sn(si)
+    new = s*bottom*si # TODO: is this correct? ... i believe it is 
+    return new
